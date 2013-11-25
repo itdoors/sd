48c7e8d7322c15812f6aa8e58b534d5b5c99eec1
Pavel <ppecheny@i.ua>
2013-11-25 22:26:59 +0200

    add ajax table

--- src/Lists/OrganizationBundle/Controller/SalesController.php
+++ src/Lists/OrganizationBundle/Controller/SalesController.php
@@ -3,12 +3,19 @@
 namespace Lists\OrganizationBundle\Controller;
 
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\Response;
 
 class SalesController extends Controller
 {
   public function indexAction()
   {
-    $page = $this->get('request')->query->get('page', 1);
+    return $this->render('ListsOrganizationBundle:Sales:index.html.twig');
+  }
+
+  public function ajaxIndexAction()
+  {
+    $page = $this->get('request')->query->get('sEcho', 1);
+    $iDisplayLength = $this->get('request')->query->get('iDisplayLength', 20);
 
     /** @var \Lists\OrganizationBundle\Entity\OrganizationRepository $organizationsRepository */
     $organizationsRepository = $this->getDoctrine()
@@ -20,14 +27,44 @@ class SalesController extends Controller
     /** @var \Knp\Component\Pager\Paginator $paginator */
     $paginator  = $this->get('knp_paginator');
 
+    /** @var \Knp\Bundle\PaginatorBundle\Pagination\SlidingPagination $pagination*/
     $pagination = $paginator->paginate(
       $organizationsQuery,
       $page,
-      20
+      $iDisplayLength
     );
 
-    return $this->render('ListsOrganizationBundle:Sales:index.html.twig', array(
-      'pagination' => $pagination,
-    ));
+    $iTotalRecords = $pagination->getTotalItemCount();
+    $iDisplayLength = $iDisplayLength < 0 ? $iTotalRecords : $iDisplayLength;
+    $iDisplayStart = 0;
+    $sEcho = $page;
+
+    $records = array();
+    $records["aaData"] = $this->ProcessArrayForAjaxJsonUotput($pagination->getItems());
+
+    $records["sEcho"] = $sEcho;
+    $records["iTotalRecords"] = $iTotalRecords;
+    $records["iTotalDisplayRecords"] = $iTotalRecords;
+
+    return new Response(json_encode($records));
+  }
+
+  /**
+   * Process array for ajax json output
+   *
+   * @param mixed[] $in
+   *
+   * @return mixed $out
+   */
+  public function ProcessArrayForAjaxJsonUotput($in)
+  {
+    $out = array();
+
+    foreach ($in as $key => $value)
+    {
+      $out[] = array_values($value);
+    }
+
+    return $out;
   }
 }
