75925c94dea318d4a1462be476799f178c3f92dd
Pavel <ppecheny@i.ua>
2013-12-03 16:55:53 +0200

    handling for all groups

--- src/Lists/OrganizationBundle/Controller/SalesAdminController.php
+++ src/Lists/OrganizationBundle/Controller/SalesAdminController.php
@@ -4,11 +4,43 @@ namespace Lists\OrganizationBundle\Controller;
 
 use Lists\OrganizationBundle\Controller\SalesController as BaseController;
 
-class SalesAdminController extends BaseController
+class SalesAdminController extends SalesController
 {
     protected $filterNamespace = 'organization.sales.filters';
     protected $filterForm = 'organizationSalesFilterForm';
     protected $baseRoute = 'lists_sales_organization_index';
 
+    public function indexAction()
+    {
+        $page = $this->get('request')->query->get('page', 1);
 
+        $filterForm = $this->processFilters();
+
+        /** @var \SD\UserBundle\Entity\User $user*/
+        $user = $this->getUser();
+
+        /** @var \Lists\OrganizationBundle\Entity\OrganizationRepository $organizationsRepository */
+        $organizationsRepository = $this->getDoctrine()
+            ->getRepository('ListsOrganizationBundle:Organization');
+
+        /** @var \Doctrine\ORM\Query */
+        $organizationsQuery = $organizationsRepository->getAllForSalesQuery(null, $this->getFilters());
+
+        /** @var \Knp\Component\Pager\Paginator $paginator */
+        $paginator  = $this->get('knp_paginator');
+
+        $pagination = $paginator->paginate(
+            $organizationsQuery,
+            $page,
+            20
+        );
+
+        return $this->render('ListsOrganizationBundle:Sales:index.html.twig', array(
+            'pagination' => $pagination,
+            'filterForm' => $filterForm->createView(),
+            'filterFormName' => $this->filterFormName,
+            'baseTemplate' => $this->baseTemplate,
+            'baseRoutePrefix' => $this->baseRoutePrefix,
+        ));
+    }
 }
--- src/Lists/OrganizationBundle/Controller/SalesDispatcherController.php
+++ src/Lists/OrganizationBundle/Controller/SalesDispatcherController.php
@@ -7,13 +7,16 @@ use Lists\OrganizationBundle\Controller\SalesController as SalesController;
 class SalesDispatcherController extends SalesController
 {
     protected $filterNamespace = 'organization.sales.dispatcher.filters';
-    protected $filterForm = 'organizationSalesDispatcherFilterForm';
+    protected $filterFormName = 'organizationSalesDispatcherFilterForm';
     protected $baseRoute = 'lists_sales_dispatcher_organization_index';
     protected $baseRoutePrefix = 'sales_dispatcher';
     protected $baseTemplate = 'SalesDispatcher';
 
     public function indexAction()
     {
+        /** @var \Lists\TeamBundle\Entity\TeamRepository $teamRepository */
+        $teamRepository = $this->get('lists_team.repository');
+
         $page = $this->get('request')->query->get('page', 1);
 
         $filterForm = $this->processFilters();
@@ -25,8 +28,13 @@ class SalesDispatcherController extends SalesController
         $organizationsRepository = $this->getDoctrine()
             ->getRepository('ListsOrganizationBundle:Organization');
 
+        /** @var \SD\UserBundle\Entity\User $user */
+        $user = $this->getUser();
+
+        $teamUserIds = $teamRepository->getMyTeamIdsByUser($user);
+
         /** @var \Doctrine\ORM\Query */
-        $organizationsQuery = $organizationsRepository->getAllForSalesQuery(null, $this->getFilters());
+        $organizationsQuery = $organizationsRepository->getAllForSalesQuery($teamUserIds, $this->getFilters());
 
         /** @var \Knp\Component\Pager\Paginator $paginator */
         $paginator  = $this->get('knp_paginator');
@@ -40,7 +48,7 @@ class SalesDispatcherController extends SalesController
         return $this->render('ListsOrganizationBundle:Sales:index.html.twig', array(
             'pagination' => $pagination,
             'filterForm' => $filterForm->createView(),
-            'filterFormName' => $this->filterForm,
+            'filterFormName' => $this->filterFormName,
             'baseTemplate' => $this->baseTemplate,
             'baseRoutePrefix' => $this->baseRoutePrefix,
         ));
