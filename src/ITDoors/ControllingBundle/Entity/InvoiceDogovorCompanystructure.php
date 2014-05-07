4d797d6409d2902a90a06f60e9455fbc90dff524
senj <senj@senj-mob>
2014-05-07 17:54:04 +0300

    добавление  Страница инвойса  (добавление класса связки талиц счета+договор+исполнитель)

---
+++ src/ITDoors/ControllingBundle/Entity/InvoiceDogovorCompanystructure.php
@@ -0,0 +1,96 @@
+<?php
+
+namespace ITDoors\ControllingBundle\Entity;
+
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * InvoiceDogovorCompanystructure
+ */
+class InvoiceDogovorCompanystructure
+{
+    /**
+     * @var integer
+     */
+    private $invoiceId;
+
+    /**
+     * @var integer
+     */
+    private $dogovorId;
+
+    /**
+     * @var integer
+     */
+    private $companystructureId;
+
+
+    /**
+     * Set invoiceId
+     *
+     * @param integer $invoiceId
+     * @return InvoiceDogovorCompanystructure
+     */
+    public function setInvoiceId($invoiceId)
+    {
+        $this->invoiceId = $invoiceId;
+    
+        return $this;
+    }
+
+    /**
+     * Get invoiceId
+     *
+     * @return integer 
+     */
+    public function getInvoiceId()
+    {
+        return $this->invoiceId;
+    }
+
+    /**
+     * Set dogovorId
+     *
+     * @param integer $dogovorId
+     * @return InvoiceDogovorCompanystructure
+     */
+    public function setDogovorId($dogovorId)
+    {
+        $this->dogovorId = $dogovorId;
+    
+        return $this;
+    }
+
+    /**
+     * Get dogovorId
+     *
+     * @return integer 
+     */
+    public function getDogovorId()
+    {
+        return $this->dogovorId;
+    }
+
+    /**
+     * Set companystructureId
+     *
+     * @param integer $companystructureId
+     * @return InvoiceDogovorCompanystructure
+     */
+    public function setCompanystructureId($companystructureId)
+    {
+        $this->companystructureId = $companystructureId;
+    
+        return $this;
+    }
+
+    /**
+     * Get companystructureId
+     *
+     * @return integer 
+     */
+    public function getCompanystructureId()
+    {
+        return $this->companystructureId;
+    }
+}
