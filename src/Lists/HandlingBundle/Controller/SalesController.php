840cc6f3bb53d40f8daeff29c96400ddaf6845db
Pavel <ppecheny@i.ua>
2014-06-16 12:31:26 +0300

    fix organization create in wizard

--- src/Lists/HandlingBundle/Controller/SalesController.php
+++ src/Lists/HandlingBundle/Controller/SalesController.php
@@ -416,6 +416,8 @@ class SalesController extends BaseController
                     $em->persist($organization);
                     $em->flush();
 
+                    $em->refresh($organization);
+
                     $organizationId = $organization->getId();
 
                     $this->setWizardOrganization(array(
