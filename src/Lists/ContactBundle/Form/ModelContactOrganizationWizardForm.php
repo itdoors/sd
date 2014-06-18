93cf842c1a92d167d60edfefa90a949fc6f4b729
senj <senj@senj-mob>
2014-06-18 14:18:13 +0300

    task 576 - count stuff

--- src/Lists/ContactBundle/Form/ModelContactOrganizationWizardForm.php
+++ src/Lists/ContactBundle/Form/ModelContactOrganizationWizardForm.php
@@ -35,32 +35,6 @@ class ModelContactOrganizationWizardForm extends ModelContactOrganizationFormTyp
         $builder
             ->remove('modelId')
             ->remove('cancel');
-
-        $builder->addEventListener(
-            FormEvents::POST_SUBMIT,
-            function (FormEvent $event) use ($container) {
-                $data = $event->getData();
-
-                $form = $event->getForm();
-
-                if ($data) {
-                    if (!$data->getPhone1() && !$data->getPhone2() && !$data->getEmail()) {
-                        $translator = $container->get('translator');
-
-                        $msg = $translator->trans("Enter Phone or Phone mob or email", array(), 'ListsContactBundle');
-
-                        $form->get('phone1')->addError(new FormError($msg));
-                    }
-                    if (!$data->getLevel()) {
-                        $translator = $container->get('translator');
-
-                        $msg = $translator->trans("Enter level", array(), 'ListsContactBundle');
-
-                        $form->get('level')->addError(new FormError($msg));
-                    }
-                }
-            }
-        );
     }
 
     /**
