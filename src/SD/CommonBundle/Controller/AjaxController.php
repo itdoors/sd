17aa2d5ec9d5ab5c709212e71a8d05d9d33d1a67
silence44 <silence4r4@mail.ru>
2014-06-20 15:14:22 +0300

    date to ukrainian format done

--- src/SD/CommonBundle/Controller/AjaxController.php
+++ src/SD/CommonBundle/Controller/AjaxController.php
@@ -3291,7 +3291,7 @@ class AjaxController extends BaseFilterController
 
         $return = array('error'=>false);
 
-        $return['value'] = $value;
+        $return['value'] = $value->format('d-m-Y');
         $return['method'] = $methodSet;
         $return['object'] = $object;
         try {
