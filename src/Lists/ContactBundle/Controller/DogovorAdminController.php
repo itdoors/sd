e2197c78b359effa5359a1239a40b9578bfe1538
senj <senj@senj-mob>
2014-06-20 12:09:11 +0300

    etask 617 - organization

---
+++ src/Lists/ContactBundle/Controller/DogovorAdminController.php
@@ -0,0 +1,12 @@
+<?php
+
+namespace Lists\ContactBundle\Controller;
+
+use Lists\ContactBundle\Controller\SalesAdminController;
+/**
+ * Class SalesAdminController
+ */
+class DogovorAdminController extends SalesAdminController
+{
+   
+}
