c041035090fa202059ec7f086b9e9a0c8b0264eb
senj <senj@mail.ru>
2014-07-20 10:32:55 +0300

    add speed

--- src/Lists/OrganizationBundle/Command/SetManagerMainCommand.php
+++ src/Lists/OrganizationBundle/Command/SetManagerMainCommand.php
@@ -10,6 +10,8 @@ use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
 use Doctrine\DBAL\Connection;
+use Lists\HandlingBundle\Entity\HandlingUser;
+use Lists\OrganizationBundle\Entity\OrganizationUser;
 
 /**
  * Class DeleteCommand
@@ -43,6 +45,9 @@ class SetManagerMainCommand extends ContainerAwareCommand
     {
         $em = $this->getContainer()->get('doctrine')->getManager();
 
+        /** @var Lookup $lookup */
+        $lookup = $em->getRepository('ListsLookupBundle:Lookup')->findOneBy(array('lukey' => 'manager_organization'));
+                
         $organizations = $em->getRepository('ListsOrganizationBundle:Organization')->findAll();
         foreach ($organizations as $organization) {
             echo $organization->getId()." \t\n";
@@ -60,8 +65,104 @@ class SetManagerMainCommand extends ContainerAwareCommand
                 echo 'In organization not found user creator and manager id: '.$organization->getId()."\n";
             }
             if ($userId) {
-                $serviceOrganizationUser = $this->getContainer()->get('lists_organization.user.service');
-                $serviceOrganizationUser->changeManagerOrganizationProject($organization->getId(), $userId);
+                $organizationId = $organization->getId();
+                
+                /** @var Organization $organization */
+                $organization = $em->getRepository('ListsHandlingBundle:Handling')->find($organizationId);
+                
+                /** @var User $user */
+                $user = $em->getRepository('SDUserBundle:User')->find($userId);
+
+                $managerOrganization = $em
+                    ->getRepository('ListsOrganizationBundle:OrganizationUser')
+                    ->findOneBy(array(
+                        'organizationId' => $organizationId,
+                        'lookupId' => $lookup->getId()
+                        ));
+
+                if (!$managerOrganization) {
+                    $managerOrganization = $em
+                        ->getRepository('ListsOrganizationBundle:OrganizationUser')
+                        ->findOneBy(array(
+                            'organizationId' => $organizationId,
+                            'userId' => $userId
+                        ));
+                    if (!$managerOrganization) {
+                        $managerOrganization = new OrganizationUser();
+                        $managerOrganization->setUser($user);
+                        $managerOrganization->setOrganization($organization);
+                    }
+                    $managerOrganization->setLookup($lookup);
+                } else {
+                    $oldManager = $em
+                        ->getRepository('ListsOrganizationBundle:OrganizationUser')
+                        ->findOneBy(array(
+                            'organizationId' => $organizationId,
+                            'userId' => $userId,
+                            ));
+                    if ($oldManager) {
+                        $em->remove($oldManager);
+                    }
+                    $managerOrganization->setUser($user);
+                }
+                $em->persist($managerOrganization);
+                
+                /** @var Handling $handlings */
+                $handlings = $em->getRepository('ListsHandlingBundle:Handling')
+                        ->findBy(array('organization_id' => $organizationId));
+                foreach ($handlings as $handling) {
+                    $handlingId = $handling->getId();
+                    $user = $em->getRepository('SDUserBundle:User')->find($userId);
+
+                    $lookup = $em->getRepository('ListsLookupBundle:lookup')->findOneBy(array('lukey' => 'manager_project'));
+
+                    $mainManager = $em
+                        ->getRepository('ListsHandlingBundle:HandlingUser')
+                        ->findOneBy(array(
+                            'handlingId' => $handlingId,
+                            'lookupId' => $lookup->getId(),
+                            ));
+
+                    if (!$mainManager) {
+                        $mainManager = $em
+                            ->getRepository('ListsHandlingBundle:HandlingUser')
+                            ->findOneBy(array(
+                                'handlingId' => $handlingId,
+                                'userId' => $userId,
+                            ));
+                        if (!$mainManager) {
+                            $mainManager = new HandlingUser();
+                            $mainManager->setUser($user);
+                            $mainManager->setHandling($handling);
+                            $mainManager->setPart(100);
+                            $mainManager->setLookup($lookup);
+                        } else {
+                            $mainManager->setLookup($lookup);
+                            $mainManager->setPart(100);
+                        }
+                    } else {
+                        $oldManager = $em
+                            ->getRepository('ListsHandlingBundle:HandlingUser')
+                            ->findOneBy(array(
+                                'handlingId' => $handlingId,
+                                'userId' => $userId,
+                                ));
+                        if ($oldManager) {
+                            $part = $mainManager->getPart()+$oldManager->getPart();
+                            $em->remove($oldManager);
+                            if ($part > 100) {
+                                $part = 100;
+                            }
+                            $mainManager->setPart($part);
+                        } else {
+                            if ($mainManager->getPart() === null) {
+                                $mainManager->setPart(100);
+                            }
+                        }
+                        $mainManager->setUser($user);
+                    }
+                    $em->persist($mainManager);
+                }
             }
         }
         $em->flush();
