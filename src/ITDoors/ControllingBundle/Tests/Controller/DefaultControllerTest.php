5f8c61f2570f4d6e23b67fb4a6b88edff8f60e35
senj <senj@mail.ru>
2014-08-06 16:55:54 +0300

    task-766-invoice-add-filter

--- src/ITDoors/ControllingBundle/Tests/Controller/DefaultControllerTest.php
+++ src/ITDoors/ControllingBundle/Tests/Controller/DefaultControllerTest.php
@@ -15,12 +15,24 @@ class DefaultControllerTest extends WebTestCase
      * testIndex
      * 
      */
+    public function test1Index()
+    {
+        $client = static::createClient();
+
+        $crawler = $client->request('GET', '/app_dev.php/controlling/invoice');
+
+        $this->assertTrue($crawler->filter('html:contains("Фильтры")')->count() > 0);
+    }
+    /**
+     * testIndex
+     * 
+     */
     public function testIndex()
     {
         $client = static::createClient();
 
-        $crawler = $client->request('GET', '/hello/Fabien');
+        $crawler = $client->request('GET', '/app_dev.php/controlling/invoice/489');
 
-        $this->assertTrue($crawler->filter('html:contains("Hello Fabien")')->count() > 0);
+        $this->assertTrue($crawler->filter('html:contains("Акт")')->count() > 0);
     }
 }
