168c8d6dccfd480e5fb0fe702b930bc1b550fad6
Pavel <ppecheny@i.ua>
2014-09-02 16:30:33 +0300

    #ami

--- src/ITDoors/SipBundle/Command/HistoryCommand.php
+++ src/ITDoors/SipBundle/Command/HistoryCommand.php
@@ -6,7 +6,6 @@
 namespace ITDoors\SipBundle\Command;
 
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
-use Symfony\Component\Console\Input\InputArgument;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Input\InputOption;
 use Symfony\Component\Console\Output\OutputInterface;
