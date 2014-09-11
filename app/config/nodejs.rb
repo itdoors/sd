d7bb2ebec3bea49c99999387d74e3c7fedb81e9b
Pavel <ppecheny@i.ua>
2014-09-11 14:17:01 +0300

    #forever node

--- app/config/nodejs.rb
+++ app/config/nodejs.rb
@@ -7,4 +7,27 @@ set :deploy_via, :remote_cache
 #role :web,        domain                          # Your HTTP server, Apache/etc
 #role :app,        domain, :primary => true       # This may be the same as your `Web` server
 
+desc "Install node modules non-globally"
+task :npm_update do
+    run "cd #{latest_release}/node/ami && npm -g update"
+end
+
+namespace :forever do
+    task :stop do
+        run "sudo forever stopall"
+    end
+
+    task :start do
+        run "cd #{latest_release}/node/ami && sudo forever start master.js"
+    end
+
+    task :restart do
+        forever_stop
+        sleep 5
+        forever_start
+    end
+end
+
 after "deploy",  "deploy:cleanup"
+after "deploy",  "npm_update"
+after "deploy",  "forever:restart"
