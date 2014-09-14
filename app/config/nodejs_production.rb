dd80f32545c3b6e3ab63c80135f8ecfbe2581074
Pavel <ppecheny@i.ua>
2014-09-14 18:06:01 +0300

    #production ci

--- app/config/nodejs_production.rb
+++ app/config/nodejs_production.rb
@@ -1,7 +1,8 @@
 server "193.242.166.21", :web, :app, :primary => true
 set :user,       "23984-5766"
 set :parameters_file, "parameters_nodejs.yml"
-set :branch, "master"
+#set :branch, "master"
+set :branch, "staging"
 
 set :deploy_via, :remote_cache
 
