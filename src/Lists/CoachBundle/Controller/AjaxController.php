0e8fabdba9720b3e83272b0bfd1e17da48c33241
topak27 <topak27@gmail.com>
2014-12-13 23:00:26 +0200

    settings tab done

--- src/Lists/CoachBundle/Controller/AjaxController.php
+++ src/Lists/CoachBundle/Controller/AjaxController.php
@@ -5,6 +5,8 @@ namespace Lists\CoachBundle\Controller;
 use ITDoors\AjaxBundle\Controller\BaseFilterController as BaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\JsonResponse;
+use Lists\CoachBundle\Entity\ActionTopic;
+use Lists\CoachBundle\Entity\ActionType;
 
 /**
  * AjaxController
@@ -35,6 +37,60 @@ class AjaxController extends BaseController
     }
 
     /**
+     * Adds/edits topic
+     *
+     * @return JsonResponse
+     */
+    public function editTopicAction(Request $request)
+    {
+        $id = $request->get('id');
+        $title = $request->get('title');
+        $em = $this->getDoctrine()->getManager();
+        $actionTopic = null;
+
+        if ($id) {
+            $actionTopic = $this
+                ->getDoctrine()
+                ->getRepository('ListsCoachBundle:ActionTopic')
+                ->find($id);
+        } else {
+            $actionTopic = new ActionTopic();
+        }
+
+        $actionTopic->setTitle($title);
+        $em->persist($actionTopic);
+        $em->flush();
+
+        $response = [
+                        'id' => $actionTopic->getId(),
+                        'title' => $actionTopic->getTitle()
+        ];
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * Delete topic
+     *
+     * @return JsonResponse
+     */
+    public function deleteTopicAction(Request $request)
+    {
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+
+        $actionTopic = $this
+            ->getDoctrine()
+            ->getRepository('ListsCoachBundle:ActionTopic')
+            ->find($id);
+
+        $em->remove($actionTopic);
+        $em->flush();
+
+        return new JsonResponse();
+    }
+
+    /**
      * Returns all possible action types
      *
      * @return JsonResponse
@@ -55,6 +111,60 @@ class AjaxController extends BaseController
         }
 
         return new JsonResponse($result);
+    }
+
+    /**
+     * Adds/edits type
+     *
+     * @return JsonResponse
+     */
+    public function editTypeAction(Request $request)
+    {
+        $id = $request->get('id');
+        $title = $request->get('title');
+        $em = $this->getDoctrine()->getManager();
+        $actionType = null;
+
+        if ($id) {
+            $actionType = $this
+                ->getDoctrine()
+                ->getRepository('ListsCoachBundle:ActionType')
+                ->find($id);
+        } else {
+            $actionType = new ActionType();
+        }
+
+        $actionType->setTitle($title);
+        $em->persist($actionType);
+        $em->flush();
+
+        $response = [
+                        'id' => $actionType->getId(),
+                        'title' => $actionType->getTitle()
+        ];
+
+        return new JsonResponse($response);
+    }
+
+    /**
+     * Delete type
+     *
+     * @return JsonResponse
+     */
+    public function deleteTypeAction(Request $request)
+    {
+        $id = $request->get('id');
+        $em = $this->getDoctrine()->getManager();
+
+        $actionType = $this
+            ->getDoctrine()
+            ->getRepository('ListsCoachBundle:ActionType')
+            ->find($id);
+
+        $em->remove($actionType);
+        $em->flush();
+
+        return new JsonResponse();
     }
 
     /**
