3d1f2795928e559583d7a9d9a3b1cb60f8385c59
senj <senj@mail.ru>
2014-12-26 19:35:22 +0200

    add stuff

--- src/SD/UserBundle/Controller/ResettingController.php
+++ src/SD/UserBundle/Controller/ResettingController.php
@@ -5,6 +5,11 @@ namespace SD\UserBundle\Controller;
 use FOS\UserBundle\Controller\ResettingController as BaseController;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\RedirectResponse;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
+use FOS\UserBundle\Event\GetResponseUserEvent;
+use FOS\UserBundle\FOSUserEvents;
+use FOS\UserBundle\Event\FormEvent;
+use FOS\UserBundle\Event\FilterUserResponseEvent;
 
 /**
  * UserController
