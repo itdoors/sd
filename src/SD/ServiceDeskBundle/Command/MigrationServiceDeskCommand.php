fdbf6e4ed2bc509a7400db8768f57977f9503c19
silence44 <silence4r4@mail.ru>
2015-02-16 19:20:39 +0200

    command added

--- src/SD/ServiceDeskBundle/Command/MigrationServiceDeskCommand.php
+++ src/SD/ServiceDeskBundle/Command/MigrationServiceDeskCommand.php
@@ -1,17 +1,23 @@
 <?php
 
 /**
- * Command class for deleting handling
+ * Command class
  */
 
 namespace SD\ServiceDeskBundle\Command;
 
+use Lists\IndividualBundle\Entity\Contact;
+use Lists\IndividualBundle\Entity\ContactType;
 use Lists\IndividualBundle\Entity\Individual;
+use SD\BusinessRoleBundle\Entity\ClaimPerformer;
 use SD\BusinessRoleBundle\Entity\CompanyClient;
 use SD\ServiceDeskBundle\Entity\Claim;
 use SD\ServiceDeskBundle\Entity\ClaimDepartment;
+use SD\ServiceDeskBundle\Entity\ClaimMessage;
+use SD\ServiceDeskBundle\Entity\ClaimPerformerRule;
 use SD\ServiceDeskBundle\Entity\ClaimType;
 use SD\ServiceDeskBundle\Entity\OrganizationGrantedForOrder;
+use SD\UserBundle\Entity\User;
 use Symfony\Bundle\FrameworkBundle\Command\ContainerAwareCommand;
 use Symfony\Component\Console\Input\InputInterface;
 use Symfony\Component\Console\Output\OutputInterface;
@@ -55,13 +61,18 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
 
         $conn = $em->getConnection();
 
+        //updating organizations for clients/
+        $stmt = $conn->prepare('
+        UPDATE client SET organization_id = (SELECT organization_id FROM client_organization WHERE client_id = client.id Limit 1)  WHERE organization_id IS NULL
+            ');
+        $stmt->execute();
+
+        $this->createIndividuals();
 
         /*
          * claim
          */
-        $stmtClaim = $conn->prepare('
-              SELECT * FROM claim
-            ');
+        $stmtClaim = $conn->prepare('SELECT * FROM claim');
         $stmtClaim->execute();
 
         $claims = $stmtClaim->fetchAll();
@@ -87,7 +98,7 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
             $claimMpk = $claim['mpk'];
 
             $stmtClaimStatus = $conn->prepare('
-              SELECT * FROM status WHERE id = ' . $claimStatusId . '
+              SELECT * FROM status WHERE id = '.$claimStatusId .'
             ');
             $stmtClaimStatus->execute();
 
@@ -101,6 +112,8 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
 
 
             //new claim here
+            $res = 'new claim';
+            $output->writeln($res);
 
             $sd_claim = new ClaimDepartment();
             switch ($claimTypeId) {
@@ -152,7 +165,12 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
             //$sd_claim->setMessages();
             //$sd_claim->setCustomer();
             //$sd_claim->addClaimPerformerRules();
+            $res = $claimType.' - '.$claimStatus.' - '.$claimCreateDatetime.' - '.$claimCloseDatetime.' - '.$claimDescription.' - '.$claimDepartmentId;
+            $output->writeln($res);
+            $res = '______________________________________';
+            $output->writeln($res);
 
+            $this->insertCommentMessage($sd_claim, $claimId, $output);
             /*
             * claim user
             */
@@ -174,6 +192,9 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                 // but firstly create them
                 //if client
                 if ($userType == 'client') {
+                    $res = 'create client-->';
+                    $output->writeln($res);
+
                     /*
                      * client_info
                      */
@@ -206,7 +227,7 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                     $userMiddleName = $user['middle_name'];
                     $userPosition = $user['position'];
                     $userEmail = $user['email_address'];
-                    $username = $user['username'];
+/*                    $username = $user['username'];
                     $userAlgorithm = $user['algorithm'];
                     $userSalt = $user['salt'];
                     $userPassword = $user['password'];
@@ -214,20 +235,45 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                     $userIsSuperAdmin = $user['is_super_admin'];
                     $userLastLogin = $user['last_login'];
                     $userSexID = $user['sex_id'];
-                    $userCreatedAt = $user['created_at'];
+                    $userCreatedAt = $user['created_at'];*/
+
+                    $res = 'create individual contacts for user -->'.$claimUser['user_id'];
+                    $output->writeln($res);
+
+                    $user = $doctrine->getRepository('SDUserBundle:User')
+                        ->find($claimUser['user_id']);
 
                     // new individual for client here
-                    $sd_individual = new Individual();
-                    $sd_individual->setMiddleName($userMiddleName);
-                    $sd_individual->setLastName($userLastName);
-                    $sd_individual->setFirstName($userFirstName);
-                    //contacts here
+                    $sd_individual = $user->getIndividual();
 
+
+                    //contacts here
+                    $contact_1 = new Contact();
+                    $contact_1->setIndividual($sd_individual);
+                    $contact_1->setType(ContactType::TEL);
+                    $contact_1->setValue($clientPhone);
+
+                    $contact_2 = new Contact();
+                    $contact_2->setIndividual($sd_individual);
+                    $contact_2->setType(ContactType::TEL);
+                    $contact_2->setValue($clientMobilephone);
+
+                    $contact_3 = new Contact();
+                    $contact_3->setIndividual($sd_individual);
+                    $contact_3->setType(ContactType::EMAIL);
+                    $contact_3->setValue($userEmail);
+
+
+                    $res = $userMiddleName.' - '.$userLastName.' - '.$userFirstName.' - '.$clientOrganizationId;
+                    $output->writeln($res);
 
                     // new client here
                     $organization = $doctrine
                         ->getRepository('ListsOrganizationBundle:Organization')
                         ->find($clientOrganizationId);
+
+                    $res = 'create Company Client-->';
+                    $output->writeln($res);
 
                     $sd_client = new CompanyClient();
                     $sd_client->setIndividual($sd_individual);
@@ -237,6 +283,8 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
 
                     if (!in_array($organization, $checkClientOrganizations->toArray())) {
                         $sd_client->addOriginOrganization($organization);
+                        $res = 'add organization :'.$organization;
+                        $output->writeln($res);
                     }
 
                     /*
@@ -260,10 +308,15 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                             ->find($departmentId);
 
                         $sd_client->addDepartment($departmentAddClient);
+                        $res = 'add department :'.$departmentAddClient;
+                        $output->writeln($res);
 
                         $checkClientGrantedOrganizations = $sd_client->getGrantedOrganizations();
                         if (!in_array($departmentAddClient->getOrganization(), $checkClientGrantedOrganizations->toArray())) {
+                            $res = 'add granted organization :'.$departmentAddClient->getOrganization();
+                            $output->writeln($res);
                             $sd_client->addGrantedOrganization($departmentAddClient->getOrganization());
+
                         }
 
                     }
@@ -282,6 +335,8 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                     foreach ($clientOrganizations as $clientOrganization) {
                         $clientId = $clientOrganization['client_id'];
                         $organizationId = $clientOrganization['organization_id'];
+                        $res = 'add client granted organization :'.$organizationId;
+                        $output->writeln($res);
 
                         $organization = $doctrine
                             ->getRepository('ListsOrganizationBundle:Organization')
@@ -290,6 +345,9 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
                         $checkClientGrantedOrganizations = $sd_client->getGrantedOrganizations();
                         if (!in_array($organization, $checkClientGrantedOrganizations->toArray())) {
                             $sd_client->addGrantedOrganization($organization);
+                            $res = 'add client granted organization :'.$organization;
+                            $output->writeln($res);
+
                         }
                     }
 
@@ -298,7 +356,45 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
 
                     //}
                 } else {
-                    //add performers here
+                    // performers here
+                    $stmtUser = $conn->prepare('
+                          SELECT * FROM fos_user WHERE id = ' . $claimUser['user_id'] . '
+                        ');
+
+                    $stmtUser->execute();
+
+                    $user = $stmtUser->fetchAll()[0];
+
+                    $user = $doctrine->getRepository('SDUserBundle:User')
+                        ->find($claimUser['user_id']);
+
+                    $res = 'create individual contacts-->';
+                    $output->writeln($res);
+
+                    // new individual for client here
+                    $sd_individual = $user->getIndividual();
+
+                    $roles = $sd_individual->getBusinessRoles();
+                    $needRole = true;
+                    foreach($roles as $role) {
+                        if ($role instanceof ClaimPerformer) {
+                            $needRole = false;
+                            $claimPerformer = $role;
+                        }
+                    }
+                    if ($needRole) {
+                        $claimPerformer = new ClaimPerformer();
+                        $claimPerformer->setIndividual($sd_individual);
+                    }
+
+                    $performerRule = new ClaimPerformerRule();
+                    $performerRule->setClaimPerformer($claimPerformer);
+                    $performerRule->setClaim($sd_claim);
+                    $performerRule->setCanEditFinanceData(false);
+                    $performerRule->setCanPostToClients(false);
+                    $performerRule->setClaimUpdated($isRead);
+
+                    //em
                 }
 
 
@@ -306,6 +402,114 @@ class MigrationServiceDeskCommand extends ContainerAwareCommand
 
         }
 
+
+    }
+
+
+    private function createIndividuals() {
+        $doctrine = $this->getContainer()->get('doctrine');
+
+        $em = $doctrine->getManager();
+
+        $conn = $em->getConnection();
+
+        $stmtUser = $conn->prepare('
+          DELETE FROM fos_user WHERE id IN (28);
+        ');
+
+        $stmtUser->execute();
+
+        $stmtUser = $conn->prepare("
+          INSERT INTO fos_user (id,username,username_canonical,email,email_canonical,enabled,salt,password,last_login,locked,expired,
+            expires_at,confirmation_token,password_requested_at,roles,credentials_expired,credentials_expire_at,first_name,last_name,middle_name,position,is_blocked,
+            is_fired,birthday,photo,peer_id,peer_password,position_id,individual_id)
+            SELECT id,username,username,email_address,email_address,true,salt,password,null,FALSE,false,
+            null,null,null, 'a:0:{}',false,null, first_name,	last_name, middle_name,position,true,
+            	null,	birthday,null,null,null,null,null FROM sf_guard_user WHERE id = 28;
+        ");
+
+        $stmtUser->execute();
+        $stmtUser = $conn->prepare('
+          DELETE FROM client WHERE user_id IN (101, 92, 257);
+        ');
+
+        $stmtUser->execute();
+        $stmtUser = $conn->prepare('
+          DELETE FROM sf_guard_user WHERE id IN (101, 92, 257);
+        ');
+
+        $stmtUser->execute();
+        $stmtUser = $conn->prepare('
+          SELECT * FROM fos_user
+        ');
+
+        $stmtUser->execute();
+
+        $users = $stmtUser->fetchAll();
+
+        foreach ($users as $user) {
+            $individual = new Individual();
+            $individual->setFirstName($user['first_name']);
+            $individual->setBirthday($user['birthday']);
+            $individual->setLastName($user['last_name']);
+            $individual->setMiddleName($user['middle_name']);
+
+            //em
+
+            $userDb = $doctrine->getRepository('SDUserBundle:User')->find($user['id']);
+            $userDb->setIndividual($individual);
+
+            //em
+        }
+
+    }
+
+    private function insertCommentMessage($claim, $oldClaimId, $output) {
+        $doctrine = $this->getContainer()->get('doctrine');
+
+        $em = $doctrine->getManager();
+
+        $conn = $em->getConnection();
+
+        $stmtComment = $conn->prepare('
+          SELECT * FROM comments WHERE claim_id = '.$oldClaimId.'
+        ');
+
+        $stmtComment->execute();
+
+        $comments = $stmtComment->fetchAll();
+
+        foreach ($comments as $comment) {
+            $user = $doctrine->getRepository('SDUserBundle:User')
+                ->find($comment['user_id']);
+
+            $message = new ClaimMessage();
+            $message->setClaim($claim);
+            $message->setCreatedAt($comment['createdatetime']);
+            $message->setText($comment['description']);
+            $message->setVisible(true);
+            $message->setUser($user);
+            $res = 'new message--> '.$comment['description'];
+            $output->writeln($res);
+
+            //em
+        }
+    }
+
+    private function addStuff() {
+        $doctrine = $this->getContainer()->get('doctrine');
+
+        $em = $doctrine->getManager();
+
+        $conn = $em->getConnection();
+
+        $stmtStuff = $conn->prepare('
+          SELECT * FROM stuff
+        ');
+
+        $stmtStuff->execute();
+
+        $stuffs = $stmtStuff->fetchAll();
 
     }
 }
