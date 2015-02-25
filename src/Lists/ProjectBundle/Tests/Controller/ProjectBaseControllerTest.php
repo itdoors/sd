966d5735e636fb08191ed24b9d529d8a3ad820e2
senj <senj@mail.ru>
2015-02-25 17:13:53 +0200

    project test

---
+++ src/Lists/ProjectBundle/Tests/Controller/ProjectBaseControllerTest.php
@@ -0,0 +1,26 @@
+<?php
+
+namespace Lists\ProjectBundle\Tests\Controller;
+
+use Symfony\Bundle\FrameworkBundle\Test\WebTestCase;
+
+class ProjectBaseControllerTest extends WebTestCase
+{
+    public function testCreate()
+    {
+        $client = static::createClient();
+        $route = $client->getContainer()->get('router');
+        $client->request('GET', $route->generate('lists_project_simple_create'));
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $pageLogin = $client->followRedirect();
+        $this->assertTrue($pageLogin->filter('button#_submit')->count() > 0);
+        $form = $pageLogin->filter('#_submit')->form();
+        $form['_username'] = 'e.drozdova';
+        $form['_password'] = '23er2fq2WEdallas23er2fq2WE';
+        $client->submit($form);
+        $this->assertTrue($client->getResponse()->isRedirect());
+        $pageProject = $client->followRedirect();
+        $this->assertTrue($client->getResponse()->isSuccessful());
+        $this->assertTrue($pageProject->filter('html:contains("Проект")')->count() > 0);
+    }
+}
